285d839d10e064191850e3f32652948a2c3727d9
Luis Gascon <luis.gcodes@gmail.com>
2024-01-21 20:49:57 -0500

    integrate sqlmodel

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine
 import sqlite3
 
 app = FastAPI()
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./dates.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 
 @app.get("/")
@@ -10,7 +16,8 @@ async def main_route():
     return {"message": "Welcome to our API!"}
 
 
-class DateCreate(BaseModel):
+class DateCreate(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     date_name: str
     location: str
 
@@ -25,25 +32,27 @@ def create_connection():
 
 
 def create_table():
-    connection = create_connection()
-    cursor = connection.cursor()
-    try:
-        cursor.execute(
-            """
-        CREATE TABLE IF NOT EXISTS dates (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        date_name TEXT NOT NULL,
-        location TEXT NOT NULL
-        )
-        """
-        )
-        connection.commit()
-    except sqlite3.Error as e:
-        print(f"Error creating table: {e}")
-    connection.close()
+    SQLModel.metadata.create_all(engine)
+
+    # connection = create_connection()
+    # cursor = connection.cursor()
+    # try:
+    #     cursor.execute(
+    #         """
+    #     CREATE TABLE IF NOT EXISTS dates (
+    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     date_name TEXT NOT NULL,
+    #     location TEXT NOT NULL
+    #     )
+    #     """
+    #     )
+    #     connection.commit()
+    # except sqlite3.Error as e:
+    #     print(f"Error creating table: {e}")
+    # connection.close()
 
 
-create_table()  # Call this function to create the table
+create_table()
 
 
 def create_date(date: DateCreate):
@@ -59,3 +68,17 @@ def create_date(date: DateCreate):
         print(f"Error inserting data: {e}")
     finally:
         connection.close()
+
+
+session = Session(engine)
+
+def seed_db():
+    data = [
+        (1, "Skate", "Outside"),
+        (2, "Cuddle", "Inside"),
+        (3, "Get Starbucks", "Outside"),
+        (4, ""),
+    ]
+
+    cur.executemany("INSERT INTO dates VALUES(?, ?, ?)", data)
+    con.commit()
